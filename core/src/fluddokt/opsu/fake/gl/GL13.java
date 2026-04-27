ce542ef39386b5eae10f451859695796aa1ad505
coconutgyat <gigabajen@gmail.com>
2026-04-27 09:56:34 +0800

    Update 1 file: GL13.java

--- core/src/fluddokt/opsu/fake/gl/GL13.java
+++ core/src/fluddokt/opsu/fake/gl/GL13.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 public class GL13 {
 
 	public static final int GL_ACTIVE_TEXTURE = GL20.GL_ACTIVE_TEXTURE;
+	public static final int GL_TEXTURE0 = GL20.GL_TEXTURE0;
 
 	public static void glActiveTexture(int texture) {
 		Gdx.gl20.glActiveTexture(texture);
